1917095e6ed954c511ba4b65d42af61832525a0f
dec <decleggal@gmail.com>
2024-11-12 15:37:25 +0100

    Code cleanup for Products test

--- src/test/java/OrderManagementTest.java
+++ src/test/java/OrderManagementTest.java
@@ -6,7 +6,6 @@ import fr.iut.exceptions.InvalidDiscountCodeException;
 import fr.iut.exceptions.OutOfStockException;
 import fr.iut.Invoice;
 import fr.iut.Order;
-import fr.iut.Product;
 import fr.iut.ShoppingCart;
 
 import java.util.ArrayList;
@@ -16,36 +15,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderManagementTest {
     // TODO: Declare products and cart
+    Product product;
     ShoppingCart cart;
 
     @BeforeEach
     void setUp() throws OutOfStockException {
-        // TODO: Initialize products and cart
-        Product product1 = new Product("product1", 10.0, 1);
-        Product product2 = new Product("product2", 20.0, 10);
-
+        product = new Product("product", 10.0, 1);
         cart = new ShoppingCart();
-        cart.addProduct(product1);
-        cart.addProduct(product2);
     }
 
-    // TODO: Implement tests for Product
+    // Tests for Product
     @Test
-    void testProduct() {
-        Product product = new Product("product", 10.0, 1);
+    void testCreateProduct() {
         assertEquals("product", product.getName());
         assertEquals(10.0, product.getPrice());
         assertEquals(1, product.getStockQuantity());
-
-        Product product2 = new Product("product2", 20.0, 0);
-        product2.decrementStock();
     }
 
-    // TODO: Implement tests for ShoppingCart
+    @Test
+    void testProductStock() {
+        product.decrementStock();
+        assertEquals(0, product.getStockQuantity());
+        product.decrementStock();
+        assertEquals(0, product.getStockQuantity());
+        product.incrementStock();
+        assertEquals(1, product.getStockQuantity());
+    }
+
+    // Tests for ShoppingCart
     @Test
     void testCart() throws OutOfStockException {
         Product product = new Product("product", 10.0, 1);
-        ShoppingCart cart = new ShoppingCart();
         cart.addProduct(product);
         assertEquals(1, cart.getItemCount());
         assertEquals(10.0, cart.getTotalPrice());
@@ -67,8 +67,6 @@ public class OrderManagementTest {
         assertEquals(products, cart.getProductList());
 
         assertThrows(OutOfStockException.class, () -> cart.addProduct(product)); //Qu'un sel stock
-
-
     }
 
     // TODO: Implement tests for Invoice
